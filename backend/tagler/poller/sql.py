53511ea7353d013bfb55494a28584ee4647ea221
Ankit Shaw <25771394+ankitshaw@users.noreply.github.com>
2021-04-18 13:37:40 +0000

    sql

--- backend/tagler/poller/sql.py
+++ backend/tagler/poller/sql.py
@@ -1,13 +1,13 @@
 import pyodbc
 
 class SqlPoller:
-    def __init__(self, server: str, port: int, driver: str, database: str, username: str, password: str, table:str, batchSize:int, logColumn:str, uidColumn: str):
+    def __init__(self, server: str, port: int, driver: str, database: str, username: str, password: str, table:str, batchSize:int, logColumn:str, statusColumn: str):
         self.conn = pyodbc.connect('DRIVER={'+driver+'};SERVER='+server+':'+str(port)+';DATABASE='+database+';UID='+username+';PWD='+ password)
         self.cursor = self.conn.cursor()
-        self.query = "SELECT TOP " + str(batchSize) + " " + logColumn + " FROM " + table + " Where " + uidColumn + " > "
+        self.query = "SELECT TOP " + str(batchSize) + " " + logColumn + " FROM " + table + " Where " + statusColumn + " = Not Processed"
     
     def poll(self):
-        self.cursor.execute(query)
+        self.cursor.execute(self.query)
         return self.cursor.fetchall()
     
     def getMaxId(self):
