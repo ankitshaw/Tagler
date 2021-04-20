c180712935e21f9fea27186fd5c62197ed70128e
Ankit Shaw <25771394+ankitshaw@users.noreply.github.com>
2021-04-20 18:28:54 +0000

    healer loader

--- backend/tagler/esloader/healer.py
+++ backend/tagler/esloader/healer.py
@@ -2,12 +2,19 @@ from haystack.document_store.elasticsearch import ElasticsearchDocumentStore
 from typing import List, Optional, Generator, Set
 import pandas as pd
 
+from os import path
+
 class KnowledgeBaseLoader:
     def __init__(self, scheme:str="http", host:str="localhost", port:int=9200, username:str="", password:str="", index:str="document"):
         self.document_store = ElasticsearchDocumentStore(scheme=scheme, host=host, port=port, username=username, password=password, index=index)
     
-    def load_csv(self, dir:str):
-        df = pd.read_csv(dir)
+    def load_csv(self, filepath:str):
+        if path.exists(filepath):
+            df = pd.read_csv(filepath)
+        else:
+            raise FileNotFoundError("The knowledge base path passed or the directory does not exists: " + filepath +
+                                    "\nPlease check if the path is correct")
+                                    
         docs = self.format_to_dict(df)
         self.document_store.write_documents(docs)
     
@@ -15,7 +22,6 @@ class KnowledgeBaseLoader:
         documents = []
         columns = df.columns
         for index, row in df.iterrows():
-            doc = {"text":""}
             for col in columns:
                 doc[col] = row[col] 
             documents.append(doc)
